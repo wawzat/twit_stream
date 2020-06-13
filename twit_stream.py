b57ce6efc03a597d31d36b357da2675cba3962be
wawzat <wawzat@gmail.com>
2020-06-13 09:08:02 -0700

    Added sentiment score

--- twit_stream.py
+++ twit_stream.py
@@ -1,12 +1,13 @@
 # Retrives top 50 trends from twitter and tweets / tweets per minute for given keywords.
-# To do: Sentiment analysis
+# Performs rudimentary sentiment scoring
 # Store Twitter API Keys and Tokens in a file named config.py
-# James S. Lucas - 20200611
+# James S. Lucas - 20200613
 import config
 import tweepy
 from sys import stdout, argv
 import datetime
 from operator import itemgetter
+import argparse
 
 # twitter API keys:
 API_KEY = config.API_KEY 
@@ -20,20 +21,59 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
 
+def get_arguments():
+    parser = argparse.ArgumentParser(
+    description='get current trends and stats for given topics from Twitter.',
+    prog='twit_stream',
+    usage='%(prog)s [-l <locations>] [-k <keywords>]',
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    g=parser.add_argument_group(title='arguments',
+          description='''   -l, --locations <trend location(s)>     optional.  enter trend locations.
+   -k, --keywords <keywords>               optional.  enter keywords to search in stream.         ''')
+    g.add_argument('-l', '--locations',
+                    type=str,
+                    default=['USA'],
+                    nargs = '*',
+                    choices = [
+                        'World', 'NYC', 'LA', 'USA'
+                        ],
+                    metavar='',
+                    dest='locations',
+                    help=argparse.SUPPRESS)
+    g.add_argument('-k', '--keywords',
+                    type=str,
+                    default=['biden', 'trump', 'dnc', 'rnc'],
+                    nargs = '*',
+                    metavar='',
+                    dest='keywords',
+                    help=argparse.SUPPRESS)
+    args = parser.parse_args()
+    return(args)
+
+
 # tweepy SteamListner Class
 class MyStreamListener(tweepy.StreamListener):
     def __init__(self, tags):
         super(MyStreamListener, self).__init__()
-        self.num_riot = 0
         self.start_time = datetime.datetime.now() 
         self.tags = tags
         self.dict_num_tweets = { i : 0 for i in self.tags}
         self.dict_tweet_rate = { i : 0 for i in self.tags}
+        self.dict_sentiment = { i : 0 for i in self.tags}
 
     def on_status(self, status):
         #print(status.text)
-        #positive_words = ['love', 'wonderful', 'best', 'great', 'superb', 'beautiful']
-        #negative_words = ['bad', 'worst', 'stupid', 'waste', 'boring', '?', '!']
+        positive_words = [
+            'beautiful', 'begin', 'best', 'better', 'great', 'hope',
+            'leader', 'love', 'positive', 'potential', 'pro', 'ready',
+            'superb', 'wonderful'
+            ]
+        negative_words = [
+            'bad', 'cheat', 'cheeto', 'creepy', 'fraud', 'less', 'lie',
+            'orange', 'stupid', 'sleepy', 'sucks',
+            'waste', 'weak', 'worst', '?'
+            ]
         elapsed_time = datetime.datetime.now() - self.start_time
         if elapsed_time.seconds > 1:
             message = ""
@@ -42,19 +82,27 @@ class MyStreamListener(tweepy.StreamListener):
             for tag in self.tags:
                 if tag.upper() in tweet.upper():
                     self.dict_num_tweets[tag] += 1
+                    for pos_word in positive_words:
+                        if pos_word.upper() in tweet.upper():
+                            self.dict_sentiment[tag] += 1
+                            break
+                    for neg_word in negative_words:
+                        if neg_word.upper() in tweet.upper():
+                            self.dict_sentiment[tag] -= 1
+                            break
                 self.dict_tweet_rate[tag] = round(self.dict_num_tweets[tag] / elapsed_time.seconds * 60)
             for tag in self.tags:
-                message = message + tag + ": " + str(self.dict_num_tweets[tag]) + " / " + str(self.dict_tweet_rate[tag]) + " | "
+                if self.dict_num_tweets[tag] != 0:
+                    sentiment_pct = round(self.dict_sentiment[tag] / self.dict_num_tweets[tag], 2)
+                else:
+                    sentiment_pct = 0
+                message = (
+                    message + tag + ": " + str(self.dict_num_tweets[tag])
+                    + " / " + str(sentiment_pct)
+                    + " / " + str(self.dict_tweet_rate[tag])
+                    + " | "
+                    )
             stdout.write("\r | " + message + "  ")
-
-    # on_direct_message isn't working for some reason but not implemented in program so no worries
-    def on_direct_message(self, status):
-        print("Entered on_direct_message()")
-        try:
-            print(status.text)
-            return True
-        except BaseException as e:
-            print("Failed on_direct_message()", str(e))    
 
     def on_error(self, status_code):
         # Status code 420 is too many connections in time period. 10 second rate limit increases exponentially for each 420 error
@@ -64,37 +112,27 @@ class MyStreamListener(tweepy.StreamListener):
             return False
 
 
-def get_trends():
-    #WOEID = 1 # World
-    #WOEID = 23424977 # USA
-    #WOEID = 2347563 # CA (Not working)
-    #WOEID = 2442047 # Los Angeles
-    #WOEID = 2347591 # New York State (Not working)
-    #WOEID = 2459115 # NYC
-    #WOEID_dict = {'World': 1, 'USA': 23424977, 'CA': 2347563, 'LA': 2442047, 'New York State': 2347591, 'NYC': 2459115}
-    #WOEID_dict = {'World': 1, 'NYC': 2459115, 'LA': 2442047, 'USA': 23424977}
-    WOEID_dict = {'USA': 23424977}
-    for k, v in WOEID_dict.items():
-        data = api.trends_place(v, '#')
+def get_trends(args):
+    WOEID_dict = {'World': 1, 'NYC': 2459115, 'LA': 2442047, 'USA': 23424977}
+    locations = args.locations
+    for location in locations:
+        data = api.trends_place(WOEID_dict.get(location), '#')
         trends = data[0]["trends"]
         # Remove trends with no Tweet volume data
         trends = filter(itemgetter("tweet_volume"), trends)
-        # Alternatively, using 0 during sorting would work as well:
-        # sorted(trends, key=lambda trend: trend["tweet_volume"] or 0, reverse=True)
         sorted_trends = sorted(trends, key=itemgetter("tweet_volume"), reverse=True)
-        #top_10_trend_names = '\n'.join(trend['name'] for trend in sorted_trends[:10])
-        #with open("trends.txt", 'w') as trends_file:
-            #print(top_10_trend_names, file=trends_file)
         names = [trend['name'] for trend in sorted_trends]
         trends_names = ' | '.join(names)
         print(" ")
-        print(k)
+        print(location)
         print("| " + trends_names + " |")
 
 
-def main(tags):
+def main():
     try:
-        get_trends()
+        args = get_arguments()
+        tags = args.keywords
+        get_trends(args)
         # Start the tweepy SteamListner as asynchronous thread.
         myStreamListener = MyStreamListener(tags)
         myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
@@ -108,11 +146,4 @@ def main(tags):
 
 
 if __name__ == "__main__":
-    tags =[]
-    if len(argv) > 1:
-        for i, arg in enumerate(argv):
-            if i > 0:
-                tags.append(arg)
-    else:
-        tags = ['floyd', 'protest', 'blm', 'covid', 'biden', 'trump', 'love']
-    main(tags)
+    main()
