e265850d381feffe1fcd965946781fad8b0b6123
wawzat <wawzat@gmail.com>
2020-06-19 20:14:00 -0700

    Modified sentiment word list

--- twit_stream.py
+++ twit_stream.py
@@ -9,7 +9,7 @@ from sys import stdout, argv
 import datetime
 from operator import itemgetter
 import argparse
-#import csv
+import csv
 
 # twitter API keys:
 API_KEY = config.API_KEY 
@@ -27,12 +27,13 @@ def get_arguments():
     parser = argparse.ArgumentParser(
     description='get current trends and stats for given topics from Twitter.',
     prog='twit_stream',
-    usage='%(prog)s [-l <locations>] [-k <keywords>]',
+    usage='%(prog)s [-l <locations>] [-k <keywords>] [-c]',
     formatter_class=argparse.RawDescriptionHelpFormatter,
     )
     g=parser.add_argument_group(title='arguments',
           description='''   -l, --locations <trend location(s)>     optional.  enter trend locations.
-   -k, --keywords <keywords>               optional.  enter keywords to search in stream.         ''')
+   -k, --keywords <keywords>               optional.  enter keywords to search in stream.
+   -c, --csv                               optional.  save tweets and score to csv file tweets.csv         ''')
     g.add_argument('-l', '--locations',
                     type=str,
                     default=['USA'],
@@ -50,23 +51,27 @@ def get_arguments():
                     metavar='',
                     dest='keywords',
                     help=argparse.SUPPRESS)
+    g.add_argument('-c', '--csv', action='store_true',
+                    dest='csv',
+                    help=argparse.SUPPRESS)
     args = parser.parse_args()
     return(args)
 
 
 # tweepy SteamListner Class
 class MyStreamListener(tweepy.StreamListener):
-    def __init__(self, tags):
+    def __init__(self, tags, args):
         super(MyStreamListener, self).__init__()
         self.start_time = datetime.datetime.now() 
         self.tags = tags
+        self.args = args
         self.dict_num_tweets = { i : 0 for i in self.tags}
         self.dict_tweet_rate = { i : 0 for i in self.tags}
         self.dict_sentiment = { i : 0 for i in self.tags}
 
     def on_status(self, status):
         #print(status.text)
-        #csv_output_file = r"D:\Users\James\OneDrive\Documents\Raspberry Pi-Matrix5\JSL Python Code\Twitter\tweets.csv"
+        csv_output_file = r"D:\Users\James\OneDrive\Documents\Raspberry Pi-Matrix5\JSL Python Code\Twitter\tweets.csv"
         row = []
         tweet_score = 0
         positive_words = [
@@ -76,9 +81,9 @@ class MyStreamListener(tweepy.StreamListener):
             ]
         negative_words = [
             'against', 'afraid', 'anyone voting', 'bad', 'bs', 'cheat', 'cheeto', 'creepy', 'crying', 'deny', 'detest', 'despite',
-            'devisive', 'embarrass', 'evil', 'fail', 'fake', 'feeble', 'fraud', 'fuck', 'garbage', 'hell', 'homophobe', 'hoax', 'idiot',
+            'devisive', 'embarrass', 'evil', 'fail', 'fake', 'feeble', 'fraud', 'garbage', 'hell', 'homophobe', 'hoax', 'idiot',
             'leftist', 'liar', 'lying', 'loser', 'losing', 'misinformation', 'not', 'outrage', 'orange', 'painful', 'pedophile',
-            'racism', 'racist', 'rapist', 'rid', 'shit', 'stupid', 'sleepy', 'sucks',
+            'racism', 'racist', 'rapist', 'rid', 'stupid', 'sleepy', 'sucks',
             'upset', 'useless', 'waste', 'weak', 'wing', 'worst'
             ]
         elapsed_time = datetime.datetime.now() - self.start_time
@@ -90,35 +95,37 @@ class MyStreamListener(tweepy.StreamListener):
                 tweet = status.text
             #words = tweet.split()
             for tag in self.tags:
-                if tag.upper() in tweet.upper():
-                    self.dict_num_tweets[tag] += 1
-                    for pos_word in positive_words:
-                        if pos_word.upper() in tweet.upper():
-                            self.dict_sentiment[tag] += 1
-                            tweet_score += 1
-                            break
-                    for neg_word in negative_words:
-                        if neg_word.upper() in tweet.upper():
-                            self.dict_sentiment[tag] -= 1
-                            tweet_score -= 1
-                            break
-                    #with open(csv_output_file, 'a', encoding='utf_8_sig', newline='') as f_output:
-                        #csv_output = csv.writer(f_output)
-                        #row.append(tag)
-                        #if tweet_score > 0:
-                            #word = pos_word
-                        #elif tweet_score < 0:
-                            #word = neg_word
-                        #else:
-                            #word = " "
-                        #row.append(word)
-                        #row.append(tweet_score)
-                        #row.append(status.author.screen_name)
-                        #row.append(status.source)
-                        #row.append(tweet)
-                        #csv_output.writerow(row)
-                        #row = []
-                        #tweet_score = 0
+                if not tweet.startswith('RT'):
+                    if tag.upper() in tweet.upper():
+                        self.dict_num_tweets[tag] += 1
+                        for pos_word in positive_words:
+                            if pos_word.upper() in tweet.upper():
+                                self.dict_sentiment[tag] += 1
+                                tweet_score += 1
+                                break
+                        for neg_word in negative_words:
+                            if neg_word.upper() in tweet.upper():
+                                self.dict_sentiment[tag] -= 1
+                                tweet_score -= 1
+                                break
+                        if self.args.csv:
+                            with open(csv_output_file, 'a', encoding='utf_8_sig', newline='') as f_output:
+                                csv_output = csv.writer(f_output)
+                                row.append(tag)
+                                if tweet_score > 0:
+                                    word = pos_word
+                                elif tweet_score < 0:
+                                    word = neg_word
+                                else:
+                                    word = " "
+                                row.append(word)
+                                row.append(tweet_score)
+                                row.append(status.author.screen_name)
+                                row.append(status.source)
+                                row.append(tweet)
+                                csv_output.writerow(row)
+                                row = []
+                                tweet_score = 0
                 self.dict_tweet_rate[tag] = round(self.dict_num_tweets[tag] / elapsed_time.seconds * 60)
             for tag in self.tags:
                 if self.dict_num_tweets[tag] != 0:
@@ -163,7 +170,7 @@ def main():
         tags = args.keywords
         get_trends(args)
         # Start the tweepy SteamListner as asynchronous thread.
-        myStreamListener = MyStreamListener(tags)
+        myStreamListener = MyStreamListener(tags, args)
         myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener, tweet_mode='extended')
         print(" ")
         myStream.filter(track=tags)
